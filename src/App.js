78ec6803fe1b69d3e67c71da0e1470900ac96c7c
william andrade <williamstrayder@gmail.com>
2020-12-13 03:04:42 -0300

    utilizando codsandbox

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
-import './App.css';
-import { ThemeProvider, createMuiTheme, makeStyles } from '@material-ui/core';
+import "./App.css";
+import React from "react";
+import { ThemeProvider, createMuiTheme, makeStyles } from "@material-ui/core";
 
 import Home from "./Home";
-import { red } from '@material-ui/core/colors';
-import { useState } from 'react';
+import { red } from "@material-ui/core/colors";
+import { useState } from "react";
 
 const useStyles = makeStyles({
-  root: {
-  }
-})
+  root: {}
+});
 function App() {
   const [darkMode, setDarkMode] = useState(false);
   const theme = createMuiTheme({
     spacing: 4,
     palette: {
-      type: darkMode ? 'dark' : 'light',
+      type: darkMode ? "dark" : "light",
       primary: {
-        main: "#f44336",
+        main: "#f44336"
       },
       secondary: {
-        main: "#3EA6FF",
+        main: "#3EA6FF"
       },
       background: {
-        default: darkMode ? '#232323':'#FFFF',
-        dark: darkMode ? '#181818': '#f4f6f8',
-        paper: darkMode ? '#232323':'#FFFF',
+        default: darkMode ? "#232323" : "#FFFF",
+        dark: darkMode ? "#181818" : "#f4f6f8",
+        paper: darkMode ? "#232323" : "#FFFF"
       }
     }
   });
